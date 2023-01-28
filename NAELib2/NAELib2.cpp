e2c20964ee683024a9798563ae9f274020af17d8
WexForgeQ <95540615+WexForgeQ@users.noreply.github.com>
2023-01-29 02:50:11 +0300

    Add files via upload

---
+++ NAELib2/NAELib2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <cstring>
+using namespace std;
+
+
+extern "C"
+{
+	void outnum(int a) {
+		std::cout << a;
+	}
+
+
+	void outstr(string ptr) {
+		SetConsoleCP(1251);
+		SetConsoleOutputCP(1251);
+		if (ptr == "") {
+			cout << endl;
+			return;
+		}
+		for (int i = 0; ptr[i] != '\0'; i++)
+			cout << ptr[i];
+	}
+
+	void outchar(char ptr) {
+		cout << ptr;
+	}
+
+
+	string concat(string ptr, string ptr2) {
+		return ptr + ptr2;
+	}
+
+	string strcp(string ptr, string ptr2) {
+		ptr = ptr2;
+		return ptr;
+	}
+}
+	
